f431d7d8d22daf974cb3f638b44913e0abb7bcf3
aggreyrc <aggreyronoh@gmail.com>
2024-07-09 18:30:44 +0300

    Corrected student grade generator

--- Student Grade Generator.js
+++ Student Grade Generator.js
@@ -8,18 +8,17 @@ function getstudentMarks(){
             break;
         }
     }
-    return marks;
+    //const studentMarks = getstudentMarks();
+    if(marks > 79){
+        console.log("Grade A");
+    }else if (marks >= 60 && marks <= 79){
+        console.log("Grade B");
+    }else if (marks > 49 && marks <= 59){
+        console.log("Grade C");
+    }else if (marks >=40 && marks <= 49){
+        console.log("Grade D");
+    }else{
+        console.log("Grade E");
+    }
 }
 
-const studentMarks = getstudentMarks();
-if(studentMarks > 79){
-    console.log("Grade A");
-}else if (studentMarks >= 60 && studentMarks <= 79){
-    console.log("Grade B");
-}else if (studentMarks >= 49 && studentMarks <= 59){
-    console.log("Grade C");
-}else if (studentMarks >=40 && studentMarks >= 49){
-    console.log("Grade D");
-}else{
-    console.log("Grade E");
-}
